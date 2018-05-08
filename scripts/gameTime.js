d5e245c52399c72533e8f14d563602760053eab9
admin <drdjakovic@gmail.com>
2018-05-08 19:43:33 -0400

    it's a start

---
+++ scripts/gameTime.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	//$(".player").draggable();
+	alert("HELLO");
+	$(".player1").hide();
+
+	$(".player1").on("click", function() {
+		console.log("clicked");
+	});
+
+	$(".player2").on("click", function() {
+		console.log("clicked");
+	});
+});
